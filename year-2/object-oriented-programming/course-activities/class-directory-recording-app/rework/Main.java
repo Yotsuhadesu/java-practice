864c04238e6932c0bfeee6e2d93bdf395a444409
Jethro Elano <elanojet@gmail.com>
2026-08-22 09:12:20 +0800

    retrieved works

--- year-2/object-oriented-programming/course-activities/class-directory-recording-app/rework/Main.java
+++ year-2/object-oriented-programming/course-activities/class-directory-recording-app/rework/Main.java
@@ -1,5 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        UserInput.showMenu();
+        int choice;
+        do {
+            UserInput.showMenu();
+            choice = UserInput.pickAction();
+            switch (choice) {
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    
+                    break;
+                case 5:
+                    
+                    break;
+                case 6:
+                    
+                    break;
+                case 7:
+                    System.out.println("Exiting Program. Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
+                    break;
+            }
+        } while (choice != 7);
     }
 }
